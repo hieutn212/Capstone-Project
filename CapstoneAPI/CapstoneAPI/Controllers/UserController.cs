0a3766e431fa1f5ad337c2dea88461ae4985b29f
Trần Nho Hiếu <hieutn212@gmail.com>
2018-01-23 10:43:11 +0700

    create user controller api

---
+++ CapstoneAPI/CapstoneAPI/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using CapstoneData.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CapstoneAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        //public IEnumerable<User> Get(int id)
+        //{
+        //    var userService = this.Service<I>
+        //}
+    }
+}
