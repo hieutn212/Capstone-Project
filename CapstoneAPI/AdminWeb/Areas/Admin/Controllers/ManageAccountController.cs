01e114785d1bfec4a31e124b9bdf74379e74f40b
QUANGNNSE61607\Nguyen Nhut Quang <quangnn0712@gmail.com>
2018-04-07 12:27:22 +0700

    Admin Web bo sung file thieu

---
+++ CapstoneAPI/AdminWeb/Areas/Admin/Controllers/ManageAccountController.cs
@@ -0,0 +1,62 @@
+﻿using CapstoneData.Models.Entities;
+using CapstoneData.Models.Entities.Services;
+using SkyWeb.DatVM.Mvc.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Services;
+
+
+namespace Wisky.Areas.Admin.Controllers
+{
+    public class ManageAccountController : Controller
+    {
+        // GET: Admin/ManageAccount
+        public ActionResult Index()
+        {
+            //ViewBag.Username = Session["Username"];
+            return View();
+        }
+
+        [WebMethod]
+        public ActionResult AddNewLicense (String username, int price)
+        {
+            try
+            {
+                int day = 0;
+                if(price == 20)
+                {
+                    day = 30;
+                }
+                if(price == 30)
+                {
+                    day = 60;
+                }
+                if(price == 40)
+                {
+                    day = 90;
+                }
+
+                IUserService userService = this.Service<IUserService>() ;
+                IHistoryService historyService = this.Service<IHistoryService>();
+                userService.AddExpireDay(username, day);
+                User user = userService.GetByUsername(username);
+                History history = new History();
+                history.Price = (float)price;
+                history.UserId = user.Id;
+                history.CreatedDate = DateTime.Now;
+                history.BuyDate = day;
+                historyService.Create(history);
+                return this.RedirectToAction("Index", "ManageAccount", new { area = "Admin" });
+                
+            }
+            catch (Exception)
+            {
+                
+            }
+            return this.RedirectToAction("Index", "ManageAccount", new { area = "Admin" });
+        }
+    }
+}
---
+++ CapstoneAPI/AdminWeb/Controllers/LicenseController.cs
@@ -0,0 +1,30 @@
+﻿using CapstoneData.Models.Entities.Services;
+using SkyWeb.DatVM.Mvc.Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wisky.Controllers
+{
+    public class LicenseController : Controller
+    {
+        // GET: Lisense
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Boolean AddNewLicense(String username, Int64 licenseDay)
+        {
+            IUserService userService = this.Service<IUserService>();
+            var user = userService.GetByUsername(username);
+            if(user != null)
+            {
+
+            }
+            return false;
+        }
+    }
+}
