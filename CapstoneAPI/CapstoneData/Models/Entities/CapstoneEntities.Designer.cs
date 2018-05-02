a6970e15424a7d0ab7a3382f1c5d17b9de835e0b
QUANGNNSE61607\Nguyen Nhut Quang <quangnn0712@gmail.com>
2018-05-02 12:23:19 +0700

    commit forr merge

--- CapstoneAPI/CapstoneData/Models/Entities/CapstoneEntities.Designer.cs
+++ CapstoneAPI/CapstoneData/Models/Entities/CapstoneEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Nguyen Nhut Quang\Desktop\Capstone.doc\Capstone-aaaaa\CapstoneAPI\CapstoneData\Models\Entities\CapstoneEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Nguyen Nhut Quang\Desktop\Capstone.doc\CapstonFinaldm\CapstoneAPI\CapstoneData\Models\Entities\CapstoneEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
